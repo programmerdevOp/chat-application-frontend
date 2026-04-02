f70368d440132dcb0df4a38d74566ff0bfcb4f8f
sumity29 <enc.sumitpal29@gmail.com>
2026-04-02 13:37:43 +0530

    adding button in join create chat

--- src/components/JoinCreateChat.jsx
+++ src/components/JoinCreateChat.jsx
@@ -29,6 +29,14 @@ const JoinCreateChat = () => {
                        className="w-full dark:bg-gray-600 px-4 py-2 border dark:border-gray-600 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
                     />
                 </div>
+
+                {/* buttons */}
+
+                <div className="flex justify-center gap-2 mt-2">
+                    <button className="px-3 py-2 dark:bg-blue-500 hover:dark:bg-blue-800 rounded-full text-amber-50">Join Room</button>  
+
+                    <button className="px-3 py-2 dark:bg-orange-500 hover:dark:bg-orange-800 rounded-full text-amber-50">Create Room</button>  
+                </div>
             </div>
 
 
