cbe775ef879dc50b634ca4e1057004f01a3c0153
sumity29 <enc.sumitpal29@gmail.com>
2026-04-02 13:26:41 +0530

    adding room id div in join create chat

--- src/components/JoinCreateChat.jsx
+++ src/components/JoinCreateChat.jsx
@@ -3,13 +3,35 @@ const JoinCreateChat = () => {
         <div className="min-h-screen flex items-center justify-center">
             <div className="p-8 w-full flex flex-col gap-5 max-w-md rounded dark:bg-gray-800">
                 <h1 className="text-2xl font-semibold text-center text-white shadow">Join Room / Create Room...</h1>
-                 {/* form div */}
+                {/* name div */}
                 <div className="">
-                <label htmlFor="" className="block font-medium mb-2 text-white">Your Name</label>
+
+                    <label htmlFor="name"
+                        className="block font-medium mb-2 text-white">
+                        Your Name
+                    </label>
+
+                    <input type="text"
+                        id="name"
+                        className="w-full dark:bg-gray-600 px-4 py-2 border dark:border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                </div>
+
+                {/* room id div */}
+                <div className="">
+                    <label htmlFor="name" 
+                      className="block font-medium mb-2 text-white">
+                        Room Id / New Room ID...
+                    </label>
+
+                    <input type="text" 
+                       id="name"
+                       className="w-full dark:bg-gray-600 px-4 py-2 border dark:border-gray-600 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                </div>
             </div>
-            </div>
-           
-            
+
+
         </div>
     )
 }
